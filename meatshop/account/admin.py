ded92f8f6a143581204780bc5d414b97862e3dce
Abbas moradi <en.moradi.66@gmail.com>
2024-02-04 21:13:37 +0330

    user admin section implementation

--- meatshop/account/admin.py
+++ meatshop/account/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from account.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.models import Group
+from account.models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'phone_number', 'is_admin')
+    list_filter = ('is_admin',)
+
+    search_fields = ('email', 'phone_number', 'full_name')
+
+    fieldsets = (
+        (None, {'fields':('email', 'phone_number', 'full_name', 'password')}),
+        ('permission', {'fields': ('is_active', 'is_admin', 'last_login')}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields':('email', 'phone_number', 'full_name', 'password1', 'password2')}),
+    )
+
+    ordering = ('full_name',)
+    filter_horizontal = ()
+
+
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
