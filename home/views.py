626d8b32a0b60420f7abb7bf9c6125160957da5e
Abbas moradi <en.moradi.66@gmail.com>
2024-02-09 00:23:07 +0330

    checking and confirming the otp code

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from accounts.models import User
+from accounts.models import User, OtpCode
+from django.utils import timezone
 import random
 
 class Home(View):
@@ -34,4 +35,25 @@ class Register(View):
             return render(request, self.reg_temp, {'alert': 'رمز هایی که وارد کرده اید با هم برابر نیستند.'})
         
         otp_code = random.randint(10000, 99999)
-        return render(request, self.otp_temp, {'code': otp_code})
+        subject = 'کد تایید ثبت نام'
+        body = f'کد تایید شما \n code:{otp_code}'
+        user_code_exists = OtpCode.objects.filter(phone_number=request.POST['phone']).exists()
+        if user_code_exists:
+            user_code_created_time = OtpCode.objects.filter(phone_number=request.POST['phone']).values_list('created', flat=True).first()
+            current_time = timezone.now()
+            time_difference = current_time - user_code_created_time
+
+            if time_difference.total_seconds() > 120:
+                user_code_exists = OtpCode.objects.filter(phone_number=request.POST['phone']).delete()
+                OtpCode.objects.create(phone_number=request.POST['phone'],
+                                       email=request.POST['email'],
+                                       code=otp_code)
+
+            else:
+                otp_code = OtpCode.objects.filter(phone_number=request.POST['phone']).values_list('code', flat=True).first()
+            
+        else:
+            OtpCode.objects.create(phone_number=request.POST['phone'],
+                                       email=request.POST['email'],
+                                       code=otp_code)
+        return render(request, self.otp_temp)
