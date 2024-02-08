00b022815647445607b220f31900a1056a836ea8
Abbas moradi <en.moradi.66@gmail.com>
2024-02-08 18:39:47 +0330

    implementation user register

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ app_name = 'home'
 
 urlpatterns = [
     path('',views.Home.as_view(), name='home'),
+    path('register/',views.Register.as_view(), name='register'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from accounts.models import User
+import random
 
 class Home(View):
     home_temp = 'index.html'
@@ -9,3 +11,27 @@ class Home(View):
     
     def post(self, request):
         pass
+
+
+class Register(View):
+    reg_temp = 'inc/register.html'
+    otp_temp = 'inc/otp-input.html'
+    success_tem = 'inc/reg-done.html'
+
+    def get(self, request):
+        return render(request, self.reg_temp)
+    
+    def post(self, request):
+        email_checker = User.objects.filter(email=request.POST['email']).exists()
+        phone_number_checker = User.objects.filter(phone_number=request.POST['phone']).exists()
+        
+        if email_checker:
+            return render(request, self.reg_temp, {'alert':'ایمیل تکراری ! ، لطفا یک ایمیل دیگر برای ثبت نام وارد کنید.'})
+        
+        if phone_number_checker:
+            return render(request, self.reg_temp, {'alert': 'شماره تلفن تکراری! ، لطفا شماره دیگری را وارد کنید.'})
+        if request.POST['password1'] != request.POST['password2']:
+            return render(request, self.reg_temp, {'alert': 'رمز هایی که وارد کرده اید با هم برابر نیستند.'})
+        
+        otp_code = random.randint(10000, 99999)
+        return render(request, self.otp_temp, {'code': otp_code})
