4135c9a76fd840897d73e9301f0d73a5167a122b
Abbas moradi <en.moradi.66@gmail.com>
2024-02-08 13:06:11 +0330

    add static and media url/root

--- meatshop/settings.py
+++ meatshop/settings.py
@@ -119,6 +119,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static_files'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'home/static'
+]
+
+# media files
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
