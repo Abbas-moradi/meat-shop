5a244a56993e10b5ec711449bdd27b81710fa959
Abbas moradi <en.moradi.66@gmail.com>
2024-02-11 17:29:41 +0330

    Category admin classes were implemented

--- product/admin.py
+++ product/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from product.models import Product
+from product.models import Product, Category
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 
-                    'price', 'discount', 
-                    'status', 'created']
+    list_display = ['name', 'description', 'price',
+                     'discount', 'status', 'created']
+    
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'created', 'status']
