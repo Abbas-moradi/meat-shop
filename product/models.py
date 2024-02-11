2e5e1606b82e3f01b031ccc8fd5ff1c4ec233495
Abbas moradi <en.moradi.66@gmail.com>
2024-02-11 17:26:46 +0330

    implementation categries models

--- product/models.py
+++ product/models.py
@@ -9,14 +9,30 @@ class Product(models.Model):
     pic = models.ImageField(upload_to='product/')
     discount = models.PositiveSmallIntegerField()
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    economic = models.BooleanField(defualt=False)
-    status = models.BooleanField(defualt=True)
-    created = models.DateTimeField(auto_add_now=True)
+    economic = models.BooleanField(default=False)
+    status = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name = 'product'
         verbose_name_plural = 'products'
         ordering = ('created',)
     
+    def __str__(self) -> str:
+        return f'{self.name}-{self.status}'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    pic = models.ImageField(upload_to='category/')
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        ordering = ('created',)
+
     def __str__(self) -> str:
         return f'{self.name}-{self.status}'
