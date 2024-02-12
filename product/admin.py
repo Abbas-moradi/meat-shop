b0d39857574f9a50316cc5b0947f05fc3e26ddfe
Abbas moradi <en.moradi.66@gmail.com>
2024-02-12 19:37:09 +0330

    slug field improve and implement

--- product/admin.py
+++ product/admin.py
@@ -4,10 +4,10 @@ from product.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 'price',
+    list_display = ['name', 'slug','description', 'price',
                      'discount', 'status', 'created']
     
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 'created', 'status']
+    list_display = ['name', 'slug','description', 'created', 'status']
--- product/models.py
+++ product/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length = 100)
-    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length = 200)
+    slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField()
     price = models.PositiveIntegerField()
     inventory = models.PositiveIntegerField()
     pic = models.ImageField(upload_to='product/')
     discount = models.PositiveSmallIntegerField()
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='products')
     economic = models.BooleanField(default=False)
     status = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -24,8 +24,9 @@ class Product(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    description = models.TextField()
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(null=True, blank=True)
     pic = models.ImageField(upload_to='category/')
     created = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=True)
@@ -36,4 +37,4 @@ class Category(models.Model):
         ordering = ('created',)
 
     def __str__(self) -> str:
-        return f'{self.name}-{self.status}'
+        return f'{self.name}'
