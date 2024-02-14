eed72aa94320243b3c6ebb667b5df5efacfd842d
Abbas moradi <en.moradi.66@gmail.com>
2024-02-14 20:31:03 +0330

    implement card add view

--- order/views.py
+++ order/views.py
@@ -8,3 +8,13 @@ class ShopingCard(View):
 
     def get(self, request):
         return render(request, self.card_temp)
+    
+
+class CardAddProduct(View):
+    temp = ''
+
+    def get(self, request):
+        pass
+    
+    def post(self, request, product_id):
+        pass
