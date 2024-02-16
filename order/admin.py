cc80e103fe62ca604055485544e6c7d325aa7206
Abbas moradi <en.moradi.66@gmail.com>
2024-02-16 12:56:55 +0330

    implementation orders model and write admin register class

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from order.models import Order
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'paid', 'total_price', 'created', 'status')
--- order/models.py
+++ order/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name='orders')
+    paid = models.BooleanField(default=False)
+    total_price = models.PositiveBigIntegerField()
+    product_number = models.PositiveIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=True)
+    on_delete = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'order'
+        verbose_name_plural = 'orders'
+        ordering = ('created',)
+    
+    def __str__(self) -> str:
+        return self.id
