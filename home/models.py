1ab2e8acf2460f59b62ab257b1b9a62aebbfd772
Abbas moradi <en.moradi.66@gmail.com>
2024-02-21 18:03:07 +0330

    implementation contact us models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactUs(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField()
+    subject = models.CharField(max_length=250)
+    message = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'contact us'
+        verbose_name_plural = 'contact us'
+        ordering = ('created',)
+
+    def __str__(self) -> str:
+        return self.subject
