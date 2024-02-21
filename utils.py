d8413d6b8c90b812b092d5a6f1c513959e26a2d3
Abbas moradi <en.moradi.66@gmail.com>
2024-02-21 10:51:32 +0330

    implementing otp code sender method

--- utils.py
+++ utils.py
@@ -13,3 +13,19 @@ def convert_to_toman(number):
     toman_number = number / 1
     toman_string = num2words(toman_number, lang='fa')
     return f'{toman_string} تومان'
+
+
+def otp_sender(phone, message):
+    client = Client("asjkdhakjhdhkajhdkja")
+
+    try:
+        message_id = client.send("+983000505", [phone], message, "summary")
+        print(message_id)
+    except Error as e:
+        print(f"Error handled => code: {e.code}, message: {e.message}")
+
+        if e.code == ResponseCode.ErrUnprocessableEntity.value:
+            for field in e.message:
+                print(f"Field: {field} , Errors: {e.message[field]}")
+    except HTTPError as e:
+        print(f"Error handled => code: {e}")
