bdbe49e4d5d607d439cf391840f83d2da12b06f3
Abbas moradi <en.moradi.66@gmail.com>
2024-02-24 21:39:52 +0330

    improve the manage section

--- managers/views.py
+++ managers/views.py
@@ -15,24 +15,43 @@ class Manage(UserPassesTestMixin, View):
         yesterday = today - timedelta(days=1)
         last_day_date = [today, yesterday]
         last_month = today.replace(day=1) - timedelta(days=1)
+        month_date_list = []
+        for single_date in (last_month + timedelta(days=n) for n in range((today - last_month).days + 1)):
+            month_date_list.append(single_date)
 
-        last_day_orders = Order.objects.filter(paid=True)
+        last_orders = Order.objects.filter(status=True)
+        total_paid_order = last_orders.filter(paid=True)
+        total_user = len(User.objects.all())
+        total_unpaid_order = len(Order.objects.filter(paid=False))
+
         last_day = []
-        for order in last_day_orders:
+        for order in last_orders:
             if order.created.date() in last_day_date:
                 last_day.append(order)
 
+        last_month = []
+        for order in last_orders:
+            if order.created.date() in month_date_list:
+                last_month.append(order)
+
         total_price_lasd_day = 0
         for lsd in last_day:
-            total_price_lasd_day += int(lsd.total_price)
-        last_month_orders = Order.objects.filter(Q(created__year=last_month.year) & 
-                                        Q(created__month=last_month.month) & 
-                                        Q(status=True) & Q(paid=True))
-        total_price_lasd_month = last_month_orders.aggregate(total_price=Sum('total_price'))['total_price']
-
+            if lsd.paid:
+                total_price_lasd_day += int(lsd.total_price)
+        
+        total_price_lasd_month = 0
+        for lsd in last_month:
+            if lsd.paid:
+                total_price_lasd_month += int(lsd.total_price)
+        
+        total_price_paid = 0
+        for lsd in total_paid_order:
+            total_price_paid += int(lsd.total_price)
 
         return render(request, self.manage_temp ,{'last_day': last_day, 'total_price_lasd_day': total_price_lasd_day,
-                                                  'last_month': last_month_orders, 'total_price_lasd_month': total_price_lasd_month})
+                                                  'last_month': last_month, 'total_price_lasd_month': total_price_lasd_month,
+                                                  'all_user': total_user, 'total_order_paid': len(total_paid_order),
+                                                  'all_price': total_price_paid, 'total_unpaid_order': total_unpaid_order})
 
     def post(self, request):
         pass
