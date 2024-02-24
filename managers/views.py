ff5b410cb13b49c534648c26dd868c35e3a0d069
Abbas moradi <en.moradi.66@gmail.com>
2024-02-24 19:54:27 +0330

    implementation manage view and temp

--- managers/views.py
+++ managers/views.py
@@ -3,13 +3,36 @@ from django.views import View
 from order.models import Order, OrderItem
 from accounts.models import User, Address
 from django.contrib.auth.mixins import UserPassesTestMixin
+from datetime import date, timedelta, datetime
+from django.db.models import Q, Sum
 
 
 class Manage(UserPassesTestMixin, View):
     manage_temp = 'manage.html'
 
     def get(self, request):
-        return render(request, self.manage_temp)
+        today = date.today()
+        yesterday = today - timedelta(days=1)
+        last_day_date = [today, yesterday]
+        last_month = today.replace(day=1) - timedelta(days=1)
+
+        last_day_orders = Order.objects.filter(paid=True)
+        last_day = []
+        for order in last_day_orders:
+            if order.created.date() in last_day_date:
+                last_day.append(order)
+
+        total_price_lasd_day = 0
+        for lsd in last_day:
+            total_price_lasd_day += int(lsd.total_price)
+        last_month_orders = Order.objects.filter(Q(created__year=last_month.year) & 
+                                        Q(created__month=last_month.month) & 
+                                        Q(status=True) & Q(paid=True))
+        total_price_lasd_month = last_month_orders.aggregate(total_price=Sum('total_price'))['total_price']
+
+
+        return render(request, self.manage_temp ,{'last_day': last_day, 'total_price_lasd_day': total_price_lasd_day,
+                                                  'last_month': last_month_orders, 'total_price_lasd_month': total_price_lasd_month})
 
     def post(self, request):
         pass
--- meatshop/settings.py
+++ meatshop/settings.py
@@ -113,13 +113,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
+TIME_ZONE = 'Asia/Tehran'
+USE_TZ = True
+
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
