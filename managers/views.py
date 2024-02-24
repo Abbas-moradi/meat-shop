4b51393106f02bcbc1633aeaac5f98f6e0b373f3
Abbas moradi <en.moradi.66@gmail.com>
2024-02-25 00:39:01 +0330

    the best improve manage view code

--- managers/views.py
+++ managers/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.views import View
 from order.models import Order, OrderItem
 from accounts.models import User, Address
@@ -11,50 +12,46 @@ class Manage(UserPassesTestMixin, View):
     manage_temp = 'manage.html'
 
     def get(self, request):
+        # Get the current date and the previous date
         today = date.today()
         yesterday = today - timedelta(days=1)
-        last_day_date = [today, yesterday]
+        # Get the last month's date range
         last_month = today.replace(day=1) - timedelta(days=1)
-        month_date_list = []
-        for single_date in (last_month + timedelta(days=n) for n in range((today - last_month).days + 1)):
-            month_date_list.append(single_date)
-
+        month_date_list = [last_month + timedelta(days=n) for n in range((today - last_month).days + 1)]
+        # Get the orders with status True
         last_orders = Order.objects.filter(status=True)
-        total_paid_order = last_orders.filter(paid=True)
-        total_user = len(User.objects.all())
-        total_unpaid_order = len(Order.objects.filter(paid=False))
+        # Get the total number of users
+        total_user = User.objects.count()
+        # Get the total number of unpaid & paid orders
+        total_unpaid_order = Order.objects.filter(paid=False).count()
+        total_paid_order = Order.objects.filter(paid=True).count()
+        # Get the orders from the last day and the last month
+        last_day = last_orders.filter(created__date__in=[today, yesterday])
+        last_month = last_orders.filter(created__date__in=month_date_list)
+        # Get the total price of the paid orders from the last day and the last month
+        total_price_lasd_day = last_day.filter(paid=True).aggregate(Sum('total_price'))['total_price__sum'] or 0
+        total_price_lasd_month = last_month.filter(paid=True).aggregate(Sum('total_price'))['total_price__sum'] or 0
+        # Get the total price of all the paid orders
+        total_price_paid = last_orders.filter(paid=True).aggregate(Sum('total_price'))['total_price__sum'] or 0
 
-        last_day = []
-        for order in last_orders:
-            if order.created.date() in last_day_date:
-                last_day.append(order)
-
-        last_month = []
-        for order in last_orders:
-            if order.created.date() in month_date_list:
-                last_month.append(order)
-
-        total_price_lasd_day = 0
-        for lsd in last_day:
-            if lsd.paid:
-                total_price_lasd_day += int(lsd.total_price)
-        
-        total_price_lasd_month = 0
-        for lsd in last_month:
-            if lsd.paid:
-                total_price_lasd_month += int(lsd.total_price)
-        
-        total_price_paid = 0
-        for lsd in total_paid_order:
-            total_price_paid += int(lsd.total_price)
 
         return render(request, self.manage_temp ,{'last_day': last_day, 'total_price_lasd_day': total_price_lasd_day,
                                                   'last_month': last_month, 'total_price_lasd_month': total_price_lasd_month,
-                                                  'all_user': total_user, 'total_order_paid': len(total_paid_order),
+                                                  'all_user': total_user, 'total_order_paid': total_paid_order,
                                                   'all_price': total_price_paid, 'total_unpaid_order': total_unpaid_order})
 
     def post(self, request):
-        pass
+        order = Order.objects.get(id=request.POST['orderid'])
+        paid_status = request.POST.get('paid_status')
+        delivery_status = request.POST.get('delivery_status')
+        paid_status = True if paid_status == 'paid' else False
+        delivery_status = True if delivery_status == 'delivered' else False
+        order.paid = paid_status
+        order.deliver = delivery_status
+        order.save()
+       
+        messages.success(request, 'تغییرات شما اعمال شد. \n لطفا در تغییرات دقت لازم را داشته باشید!')
+        return redirect('manager:manage')
 
     def test_func(self) -> bool | None:
         return self.request.user.is_authenticated and self.request.user.is_admin
