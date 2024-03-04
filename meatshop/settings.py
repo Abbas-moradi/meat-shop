9a8d175d79305262f70d963533e22c7c512e0438
Abbas moradi <en.moradi.66@gmail.com>
2024-03-04 22:28:09 +0330

    convert data base as sqlite3 to postgresql

--- meatshop/settings.py
+++ meatshop/settings.py
@@ -84,13 +84,27 @@ WSGI_APPLICATION = 'meatshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'meat-shop',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('DATA_BASE_PASS'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
